661f87b9cdab9d3715ee88660bb484d938ff1505
Valeriia <cvetlya4ek@gmail.com>
2023-10-27 16:48:21 +0200

    new commit

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 let menutoggle = document.querySelector('.menu_toggle');
 let header = document.querySelector('header');
 
-menutoggle.onclick = function(){
-    header.classList.toggle('active');
+menutoggle.onclick = function () {
+  header.classList.toggle('active');
 }
 // Get the button
 let mybutton = document.getElementById("myBtn");
 
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -32,3 +32,10 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+const toggle = document.getElementById('toggle1');
+const header2 = document.getElementById('header2');
+
+toggle.addEventListener("click", () => {
+  header2.classList.toggle("close");
+})
